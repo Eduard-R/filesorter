cd40573be94b1f2bd7c8cf77d06c48673451ee40
Eduard-R <edi_r@gmx.de>
2025-09-22 20:31:56 +0200

    bugfix

--- filesorter.py
+++ filesorter.py
@@ -6,14 +6,13 @@ import sys
 def file_sort(path):
     path = Path(path)
     for file in path.iterdir():
-        if file.is_file():
-            suffix = file.suffix.lower().strip(".")
-            if not suffix:
-                suffix = "rest"
+        suffix = file.suffix.lower().strip(".")
+        if not suffix:
+            suffix = "rest"
+        
+        destination = path / suffix
+        destination.mkdir(exist_ok=True)
 
-            destination = path / suffix
-            destination.mkdir(exist_ok=True)
+        shutil.move(str(file), destination / file.name)
 
-            shutil.move(str(file), destination / file.name)
-
-            file_sort(Path(sys.executable).parent)
+file_sort(Path(sys.executable).parent)
