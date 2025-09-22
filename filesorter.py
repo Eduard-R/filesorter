67f92efaa219491ce3c48804ddf3622879abc941
Eduard-R <edi_r@gmx.de>
2025-09-22 20:28:14 +0200

    developing

---
+++ filesorter.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+import shutil
+import sys
+
+def file_sort(path):
+    path = Path(path)
+    for file in path.iterdir():
+        if file.is_file():
+            suffix = file.suffix.lower().strip(".")
+            if not suffix:
+                suffix = "rest"
+
+            destination = path / suffix
+            destination.mkdir(exist_ok=True)
+
+            shutil.move(str(file), destination / file.name)
+
+            file_sort(Path(sys.executable).parent)
